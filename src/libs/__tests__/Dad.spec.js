506770abfed66a070feda71fd8636a50d7f315f1
gnituy18 <sourfeethsu@gmail.com>
2017-12-19 11:18:44 +0800

    Modify Dad spec definition

--- src/libs/__tests__/Dad.spec.js
+++ src/libs/__tests__/Dad.spec.js
@@ -1,40 +1,31 @@
 import dad, { tw, ja, indo } from '../../';
 
-describe('Dad Spec', () => {
-  it('test tw', () => {
-    expect(dad('2017-12-13').tw.time)
+describe('Dad functionality', () => {
+  it('return time in ms where time zone is +8:00', () => {
+    expect(dad('2017-12-13').tw)
       .toBe(new Date('2017-12-13 00:00:00+08:00').getTime());
-
-    expect(dad('2017-12-13').tw.unix)
-      .toBe(new Date('2017-12-13 00:00:00+08:00').getTime() / 1000);
 
     expect(tw('2017-12-13'))
       .toMatchObject(dad('2017-12-13').tw);
   });
 
-  it('test ja', () => {
+  it('return time in ms where time zone is +9:00', () => {
     expect(dad('2017-12-13').ja.time)
       .toBe(new Date('2017-12-13 00:00:00+09:00').getTime());
-
-    expect(dad('2017-12-13').ja.unix)
-      .toBe(new Date('2017-12-13 00:00:00+09:00').getTime() / 1000);
 
     expect(ja('2017-12-13'))
       .toMatchObject(dad('2017-12-13').ja);
   });
 
-  it('test indo', () => {
+  it('return time in ms where time zone is +7:00', () => {
     expect(dad('2017-12-13').indo.time)
       .toBe(new Date('2017-12-13 00:00:00+07:00').getTime());
-
-    expect(dad('2017-12-13').indo.unix)
-      .toBe(new Date('2017-12-13 00:00:00+07:00').getTime() / 1000);
 
     expect(indo('2017-12-13'))
       .toMatchObject(dad('2017-12-13').indo);
   });
 
-  it('invalid date format', () => {
+  it('throw an error if date format is incorrect', () => {
     expect(() => {
       dad('2017/12/13');
     })
