cda75e9db93e002348ce423988af9229c8e93c45
Charlie Hsu <sourfeethsu@gmail.com>
2018-01-12 22:54:38 +0800

    Fix unifyTimeFormat and add test to it

--- src/utils/__tests__/unifyTimeFormat.spec.js
+++ src/utils/__tests__/unifyTimeFormat.spec.js
@@ -1,8 +1,13 @@
 import unifyTimeFormat from '../unifyTimeFormat';
 
 describe('unifyTimeFormat spec', () => {
-  it('change unify time format', () => {
+  it('transform incomplete time to complete timeformat and replace space with T', () => {
     const dateString = '2018-01-01 19:00';
     expect(unifyTimeFormat(dateString)).toBe('2018-01-01T19:00:00');
+  });
+
+  it('transform incomplete time to complete timeformat', () => {
+    const dateString = '2018-01-01';
+    expect(unifyTimeFormat(dateString)).toBe('2018-01-01T00:00:00');
   });
 });
