d479c411527f277b169a53a1f01c358f5255302b
Hsu Yu-Ting <sourfeethsu@gmail.com>
2018-01-30 10:48:18 +0800

    Change secToDate returns and typo (#14)
    * Change secToDate returns and typo
    
    * Change naming
    
    * Update README.md

--- src/utils/__tests__/secToDate.spec.js
+++ src/utils/__tests__/secToDate.spec.js
@@ -4,18 +4,32 @@ describe('`secToDate` Spec', () => {
   it('test if timezone +8', () => {
     const s = new Date('2018-01-04 08:00:00+08:00') / 1000;
 
-    expect(new Date(s * 1000).toISOString()).toBe('2018-01-04T00:00:00.000Z');
-
     expect(
-      secToDate(8)(s).toISOString()
-    ).toBe('2018-01-04T08:00:00.000Z');
+      secToDate(8)(s)
+    ).toEqual({
+      year: 2018,
+      month: 0,
+      date: 4,
+      hours: 8,
+      minutes: 0,
+      seconds: 0,
+      day: 4,
+    });
   });
 
   it('test if timezone +9', () => {
     const s = new Date('2018-01-04 08:00:00+08:00') / 1000;
 
     expect(
-      secToDate(9)(s).toISOString()
-    ).toBe('2018-01-04T09:00:00.000Z');
+      secToDate(9)(s)
+    ).toEqual({
+      year: 2018,
+      month: 0,
+      date: 4,
+      hours: 9,
+      minutes: 0,
+      seconds: 0,
+      day: 4,
+    });
   });
 });
